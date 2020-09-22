b117e3be9ef9b10629b69ffa38e99d03c9515d41
hector galicia milla <hectormilla8@gmail.com>
2020-09-22 13:32:30 -0400

    added prototype that will ask question along with answers in console

--- js/index.js
+++ js/index.js
@@ -4,7 +4,13 @@ let QuestionConstructor = function (question, answerChoices, correctAnswers) {
     this.answerChoices = answerChoices,
     this.correctAnswers = correctAnswers
 }
+QuestionConstructor.prototype.ask = function () {
+        console.log(this.question);
+        for (i = 0; i < this.answerChoices.length; i++) {
+            console.log( this.answerChoices[i])
+        }
 
+}
 const question1 = new QuestionConstructor('What is 12 x 12?', ['0: 233', '1: 100', '2: 144'], 2);
 const question2 = new QuestionConstructor('In what year did the US get its independence?', ['0: 1800', '1: 1776', '2: 1920'], 1);
 const question3 = new QuestionConstructor('How many parts Oxygen is water of?', ['0: 1', '1: 2', '2: 3'], 0);
@@ -15,9 +21,12 @@ const question4 = new QuestionConstructor('How many feet are in a mile?', ['0: 2
 let allQuestions = [question1, question2, question3, question4]
 
 const askQuestion = function (arr) {
-     let questionNumber = Math.floor(Math.random() * 4 + 1)
-     console.log(arr[questionNumber].question)
+     let questionNumber = Math.floor(Math.random() * 4)
+     arr[questionNumber].ask()
+   
      
 }
 
 askQuestion(allQuestions)
+
+prompt('hi')
