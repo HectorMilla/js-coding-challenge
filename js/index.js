fa98a42e3095c4ac5ea3801c7bc837b4ed807060
hector galicia milla <hectormilla8@gmail.com>
2020-09-22 12:49:13 -0400

    created question constructort

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// coding challenge
+let QuestionConstructor = function (question, answerChoices, correctAnswers) {
+    this.question = question,
+    this.answerChoices = answerChoices,
+    this.correctAnswers = correctAnswers
+}
+
