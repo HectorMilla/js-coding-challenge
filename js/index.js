38b28eee8ffe3a866af9689068ac10d1570fe20f
hector galicia milla <hectormilla8@gmail.com>
2020-09-22 12:57:38 -0400

    created 4 different questions

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ let QuestionConstructor = function (question, answerChoices, correctAnswers) {
     this.correctAnswers = correctAnswers
 }
 
+const question1 = new QuestionConstructor('What is 12 x 12?', ['0: 233', '1: 100', '2: 144'], 2);
+const question2 = new QuestionConstructor('What is 12 x 12?', ['0: 233', '1: 100', '2: 144'], 2);
+const question3 = new QuestionConstructor('What is 12 x 12?', ['0: 233', '1: 100', '2: 144'], 2);
+const question4 = new QuestionConstructor('What is 12 x 12?', ['0: 233', '1: 100', '2: 144'], 2);
+
+console.log(question1.question);
